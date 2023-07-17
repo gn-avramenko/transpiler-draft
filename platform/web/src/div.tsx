2fabe716367af1095232cfaed4362fa09021b67a
Vladimir Avramenko <vladimir.avramenko@grindine.com>
2023-07-18 00:01:50 +0300

    draft

--- platform/web/src/div.tsx
+++ platform/web/src/div.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ReactWebComponent } from './base';
 
 // eslint-disable-next-line import/prefer-default-export
